bb8b48e0d04ee03723a2ad3e387690bc1be6e64b
ClemenF <clemen.fayol@gmail.com>
2019-11-15 17:59:21 +0100

    add_module

--- test.js
+++ test.js
@@ -1,40 +1,7 @@
 
 var http = require('http')
 
-const url = require('url')
-const sq = require('querystring')
+const handles = require('./module');
 
-const content = '<!DOCTYPE html>' +
-'<html>' +
-'    <head>' +
-'        <meta charset="utf-8" />' +
-'        <title>ECE AST</title>' +
-'    </head>' +
-'    <body>' +
-'         <p>Hello World !</p>' +
-'    </body>' +
-'</html>'
-
-const serverHandle = function (req, res) {
-  const route = url.parse(req.url);
-  const path = route.pathname;
-  const params = sq.parse(route.query);
-  console.log(params);
-
-
-  //const queryParams = sq.parse(url.parse(req.url).query);
-
-
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  console.log(params.name);
-  console.log(params.name);
-  if(path === '/Hello' && params['name']) {
-    res.write('Hello ' + params['name'])
-  } else {
-    res.write('Hello anonymous')
-  }
-  res.end();
-}
-
-const server = http.createServer(serverHandle);
+const server = http.createServer(handles.serverHandle);
 server.listen(8081)
